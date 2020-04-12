7d12c73307c8a58eb19384b62e7434fc92df91fc
Pedro Costa <pedrodc23@gmail.com>
2020-04-12 22:50:18 +0100

    added photo locations

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Urls from './components/Urls'
+
+function App() {
+  return (
+    <div className="App">
+      <Urls />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Urls.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react'
+
+import { Button, Checkbox, Icon, Table } from 'semantic-ui-react'
+
+
+function Urls() {
+
+    return (
+      
+    );
+  }
+  
+  export default Urls;
+  
